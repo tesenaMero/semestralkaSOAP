b579ec2398d90bcdfe32c5535cc33cdae66216ea
Jan Pippal <jan.pippal@tesena.com>
2017-06-04 18:45:26 +0200

    soap ui xml file

--- src/main/java/io/spring/guides/gs_producing_web_service/CreateCountryRequest.java
+++ src/main/java/io/spring/guides/gs_producing_web_service/CreateCountryRequest.java
@@ -6,10 +6,7 @@
 package io.spring.guides.gs_producing_web_service;
 
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(
